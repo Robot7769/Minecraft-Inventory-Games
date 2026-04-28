270fb36ce4f4d1b6b241b770b6ba88e1eb06bfa7
Robot7769 <jan.robot7@gmail.com>
2026-04-28 17:35:26 +0200

    feat: Add Slot Machine and Tetris minigames to InvGames plugin

--- src/main/java/me/robot7769/InvGames/commands/GameCommand.java
+++ src/main/java/me/robot7769/InvGames/commands/GameCommand.java
@@ -1,11 +1,14 @@
 package me.robot7769.InvGames.commands;
 
 import me.robot7769.InvGames.games.SnakeGame;
+import me.robot7769.InvGames.games.SlotMachineGame;
+import me.robot7769.InvGames.games.TetrisGame;
 import me.robot7769.InvGames.manager.GameManager;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Locale;
 
@@ -18,7 +21,7 @@ public class GameCommand implements CommandExecutor {
     }
 
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String @NotNull [] args) {
         if (!(sender instanceof Player player)) {
             sender.sendMessage("Tento prikaz muze pouzit jen hrac.");
             return true;
@@ -38,6 +41,13 @@ public class GameCommand implements CommandExecutor {
             case "snake":
                 gameManager.startGame(player, new SnakeGame(player));
                 player.sendMessage("Spoustim hru Snake.");
+                break;
+            case "slot", "slots", "slotmachine":
+                gameManager.startGame(player, new SlotMachineGame(player));
+                player.sendMessage("Spoustim hru Slot Machine.");
+                break;
+            case "tetris":
+                gameManager.startGame(player, new TetrisGame(player));
                 break;
             default:
                 player.sendMessage("Neznama hra: " + args[0]);
